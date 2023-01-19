1b90c93044860b3f62da9e847acb2d69770e4e97
finotellicarlos <carlos.finotelli@outlook.com>
2023-01-19 13:41:28 -0300

    feat: cleaning a project and extend background color to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { Habit } from './components/Habit'
+// import { Habit } from './components/Habit'
 import './styles/global.css'
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Habit completed={13} />
-      <Habit completed={22} />
-      <Habit completed={44} />
-    </>
-  )
+  return <></>
 }
 
 export default App
