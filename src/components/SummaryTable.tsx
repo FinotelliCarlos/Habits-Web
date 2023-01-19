03b55d76e7c85818a51be5729d0b2671ee24f5a3
finotellicarlos <carlos.finotelli@outlook.com>
2023-01-19 15:32:56 -0300

    feat: add a table of summary of days

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,44 @@
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+import { HabitDay } from './HabitDay'
+
+const WEEK_DAYS = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const SUMMARY_DATES = generateDatesFromYearBeginning()
+
+const MINIMUM_SUMMARY_DATES_SIZE = 22 * 7
+
+const AMOUNT_OF_DAYS_TO_FILL = MINIMUM_SUMMARY_DATES_SIZE - SUMMARY_DATES.length
+
+export const SummaryTable = (): JSX.Element => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3 ">
+        {WEEK_DAYS.map((weekDay, index) => {
+          return (
+            <div
+              key={`${weekDay}-${index}`}
+              className="text-zinc-400 text-xl h-1- w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3 overflow-auto">
+        {SUMMARY_DATES.map(date => {
+          return <HabitDay key={date.toString()} />
+        })}
+
+        {AMOUNT_OF_DAYS_TO_FILL > 0 &&
+          Array.from({ length: AMOUNT_OF_DAYS_TO_FILL }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            )
+          })}
+      </div>
+    </div>
+  )
+}
